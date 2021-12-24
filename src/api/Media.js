65e31fcafc94032832b81ef441132fa4901c43ce
Teresa Pelinski <teresapelinski@gmail.com>
2021-12-24 18:02:29 +0100

    isDesktop hoop and title animations

---
+++ src/api/Media.js
@@ -0,0 +1,19 @@
+import React, { useContext, useState } from "react";
+import { useWindowResize, useThrottledFn } from 'beautiful-react-hooks';
+
+export const WindowSizeContext = React.createContext();
+
+export const WindowSizeReporter = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useWindowResize(useThrottledFn((event) => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }));
+    return [width, height]
+};
+export const isDesktopView = () => {
+    const { width, _ } = useContext(WindowSizeContext);
+    return width > 812;
+};
