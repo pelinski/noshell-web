7e8487bdf2993c50251668f5e2e6745f8af163f6
Teresa Pelinski <teresapelinski@gmail.com>
2022-01-01 15:30:59 +0100

    removed unused packages, moved packages to dev dependencies, ran prettier with prettierrc

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,50 +1,90 @@
-import React from "react";
+import React from 'react'
 
-export const ScrollButton = ({ scrollTo, flip = false }) =>
-    <a onClick={scrollHandler(scrollTo)} className={flip ? "button flip" : "button"}>
-        _____<span>/\\\</span>________<span>/\\\</span>______<br />
-        &nbsp;____<span>\/\\\</span>_______<span>\/\\\</span>______<br />
-        &nbsp;&nbsp;____<span>\//\\\</span>______<span>/\\\</span>_______<br />
-        &nbsp;&nbsp;&nbsp;_____<span>\//\\\</span>____<span>/\\\</span>________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;______<span>\//\\\</span>__<span>/\\\</span>_________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_______<span>\//\\\/\\\</span>__________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________<span>\//\\\\\</span>___________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\//\\\</span>____________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________<span>\///</span>_____________<br />
-    </a >
+export const ScrollButton = ({ scrollTo, flip = false }) => (
+	<a onClick={scrollHandler(scrollTo)} className={flip ? 'button flip' : 'button'}>
+		_____<span>/\\\</span>________<span>/\\\</span>______
+		<br />
+		&nbsp;____<span>\/\\\</span>_______<span>\/\\\</span>______
+		<br />
+		&nbsp;&nbsp;____<span>\//\\\</span>______<span>/\\\</span>_______
+		<br />
+		&nbsp;&nbsp;&nbsp;_____<span>\//\\\</span>____<span>/\\\</span>________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;______<span>\//\\\</span>__<span>/\\\</span>_________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_______<span>\//\\\/\\\</span>__________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________<span>\//\\\\\</span>___________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\//\\\</span>____________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________<span>\///</span>_____________
+		<br />
+	</a>
+)
 
-export const ScrollDownLargeButton = ({ scrollTo }) =>
-    <a onClick={scrollHandler(scrollTo)} className="button">
-        __________<span>/\\\</span>_____________<br />
-        &nbsp;_________<span>\/\\\</span>_____________<br />
-        &nbsp;&nbsp;_________<span>\/\\\</span>_____________<br />
-        &nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\///</span>______________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_____<span>/\\\</span>________<span>/\\\</span>______<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;____<span>\/\\\</span>_______<span>\/\\\</span>______<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;____<span>\//\\\</span>______<span>/\\\</span>_______<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_____<span>\//\\\</span>____<span>/\\\</span>________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;______<span>\//\\\</span>__<span>/\\\</span>_________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_______<span>\//\\\/\\\</span>__________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________<span>\//\\\\\</span>___________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\//\\\</span>____________<br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\///</span>______________<br />
-    </a >
+export const ScrollDownLargeButton = ({ scrollTo }) => (
+	<a onClick={scrollHandler(scrollTo)} className='button'>
+		__________<span>/\\\</span>_____________
+		<br />
+		&nbsp;_________<span>\/\\\</span>_____________
+		<br />
+		&nbsp;&nbsp;_________<span>\/\\\</span>_____________
+		<br />
+		&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\/\\\</span>_____________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________<span>\///</span>______________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_____<span>/\\\</span>________
+		<span>/\\\</span>______
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;____<span>\/\\\</span>_______
+		<span>\/\\\</span>______
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;____<span>\//\\\</span>
+		______<span>/\\\</span>_______
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_____
+		<span>\//\\\</span>____<span>/\\\</span>________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;______
+		<span>\//\\\</span>__<span>/\\\</span>_________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_______
+		<span>\//\\\/\\\</span>__________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________
+		<span>\//\\\\\</span>___________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________
+		<span>\//\\\</span>____________
+		<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_________
+		<span>\///</span>______________
+		<br />
+	</a>
+)
 
-
-export const ScrollBasicButton = ({ scrollTo, flip }) =>
-    <a onClick={scrollHandler(scrollTo)} className={flip ? "button basic-button flip" : "button basic-button"}>
-        | <br />
-        | <br />
-        | <br />
-        | <br />
-        v < br />
-    </a >
-
+export const ScrollBasicButton = ({ scrollTo, flip }) => (
+	<a
+		onClick={scrollHandler(scrollTo)}
+		className={flip ? 'button basic-button flip' : 'button basic-button'}
+	>
+		| <br />
+		| <br />
+		| <br />
+		| <br />
+		v <br />
+	</a>
+)
 
 const scrollHandler = (scrollTo) => () =>
-    document.getElementById(scrollTo)?.scrollIntoView({ behavior: "smooth" })
+	document.getElementById(scrollTo)?.scrollIntoView({ behavior: 'smooth' })
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom'
 
 export const Nav = () => {
-    const isRead = useLocation().pathname == "/read";
-    const isVideo = useLocation().pathname == "/video";
+	const isRead = useLocation().pathname == '/read'
+	const isVideo = useLocation().pathname == '/video'
 
-
-
-    return (
-        <nav>
-            {!isRead && <Link to='/read'>++read more--</Link>}
-            {!isVideo && <Link to='/video'>++a video in which i talk about this--</Link>}
-            <Link to='/me'>++about me--</Link>
-            <Link to='/'>++thx for reading :-)--</Link>
-        </nav>)
+	return (
+		<nav>
+			{!isRead && <Link to='/read'>++read more--</Link>}
+			{!isVideo && <Link to='/video'>++a video in which i talk about this--</Link>}
+			<Link to='/me'>++about me--</Link>
+			<Link to='/'>++thx for reading :-)--</Link>
+		</nav>
+	)
 }
 
-export const HomeNav = ({ expandIntro }) =>
-    <nav className='home-nav'>
-        {!expandIntro && <p>click to trigger text</p>}
-        <Link to='/read'>++read more--</Link>
-        <Link to='/me'>++about me--</Link>
-        <Link to='/'>++thx for reading :-)--</Link>
-    </nav>
+export const HomeNav = ({ expandIntro }) => (
+	<nav className='home-nav'>
+		{!expandIntro && <p>click to trigger text</p>}
+		<Link to='/read'>++read more--</Link>
+		<Link to='/me'>++about me--</Link>
+		<Link to='/'>++thx for reading :-)--</Link>
+	</nav>
+)
