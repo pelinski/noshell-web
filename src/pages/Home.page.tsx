0189eb8d40fbc40e35db0776f3d677b9c5eb2563
Teresa Pelinski <teresapelinski@gmail.com>
2022-01-02 23:14:36 +0100

    simple styled player

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -27,11 +27,103 @@ export const HomePage: React.FC = (): JSX.Element => {
 				onKeyDown={(e) => arrowsHandler({ e, prev: 'title-box', next: 'intro' })}
 			>
 				<ScrollBasicButton scrollTo={'title-box'} flip={true} />
-				listen
+				<AudioPlayer audioSrc={'audio/just_a_voice_8.wav'} />
 				<ScrollBasicButton scrollTo={'intro'} />
 			</div>
 			<Intro />
 		</>
+	)
+}
+const AudioPlayer: React.FC<{ audioSrc: string }> = ({ audioSrc }): JSX.Element => {
+	const [isPlaying, setIsPlaying] = useState<boolean>(false)
+
+	//refs
+	const audioRef = useRef<HTMLAudioElement>(new Audio(audioSrc))
+	const [trackProgress, setTrackProgress] = useState<number>(0)
+	const intervalRef = useRef<any>(null)
+
+	const { duration } = audioRef.current
+
+	const startTimer = (): void => {
+		// Clear any timers already running
+		clearInterval(intervalRef.current)
+
+		intervalRef.current = setInterval((): void => {
+			if (audioRef.current.ended) {
+				console.log(audioRef.current.currentTime)
+			} else {
+				setTrackProgress(audioRef.current.currentTime)
+			}
+		}, [1000])
+	}
+
+	useEffect(() => {
+		if (isPlaying) {
+			audioRef.current.play()
+			startTimer()
+		} else {
+			clearInterval(intervalRef.current)
+			audioRef.current.pause()
+		}
+	}, [isPlaying])
+
+	useEffect(() => {
+		// Pause and clean up on unmount
+		return (): void => {
+			audioRef.current.pause()
+			clearInterval(intervalRef.current)
+		}
+	}, [])
+
+	const onScrub = (value: string | number): void => {
+		// Clear any timers already running
+		clearInterval(intervalRef.current)
+		audioRef.current.currentTime = Number(value)
+		setTrackProgress(audioRef.current.currentTime)
+	}
+
+	const onScrubEnd = (): void => {
+		// If not already playing, start
+		if (!isPlaying) {
+			setIsPlaying(true)
+		}
+		startTimer()
+	}
+	return (
+		<div className='audio-player'>
+			<div className='audio-controls'>
+				{isPlaying ? (
+					<button
+						type='button'
+						className='pause'
+						onClick={() => setIsPlaying(false)}
+						aria-label='Pause'
+					>
+						<img src={require('url:~public/icons/pause.svg')} alt='pause button' />
+					</button>
+				) : (
+					<button
+						type='button'
+						className='play'
+						onClick={() => setIsPlaying(true)}
+						aria-label='Play'
+					>
+						<img src={require('url:~public/icons/play.svg')} alt='play button' />
+					</button>
+				)}
+			</div>
+			<input
+				type='range'
+				value={trackProgress}
+				step='1'
+				min='0'
+				max={duration ? duration : `${duration}`}
+				className='progress'
+				onChange={(e) => onScrub(e.target.value)}
+				onMouseUp={onScrubEnd}
+				onKeyUp={onScrubEnd}
+			/>
+		</div>
 	)
 }
 
